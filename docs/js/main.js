f7bc9772cb803e95c8d62fb7e04aa3cf08f06781
Alexander Romberg <adze@gmx.ch>
2021-05-01 21:49:50 +0200

    updated index-page

---
+++ docs/js/main.js
@@ -0,0 +1,27 @@
+// buttons
+const AtomAniBtn = document.getElementById('AtomAniBtn');
+const M152Btn = document.getElementById('M152Btn');
+// divs
+const welcomeDiv = document.getElementById('welcome');
+const AtomAniDiv = document.getElementById('AtomAni');
+const M152Div = document.getElementById('M152');
+
+AtomAniBtn.addEventListener('click', () => {
+    AtomAniDiv.classList.toggle('hidden');
+    M152Div.classList.add('hidden');
+    toggelMainDiv();
+});
+
+M152Btn.addEventListener('click', () => {
+    M152Div.classList.toggle('hidden');
+    AtomAniDiv.classList.add('hidden');
+    toggelMainDiv();
+});
+
+function toggelMainDiv() {
+    if (AtomAniDiv.classList.contains('hidden') && M152Div.classList.contains('hidden')) {
+        welcomeDiv.classList.remove('hidden');
+    } else {
+        welcomeDiv.classList.add('hidden');
+    }
+}
